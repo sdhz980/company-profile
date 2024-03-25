0dfd868d9c461b5e1f8f940c7d71f072d07263c9
sdhz980 <sadamhack@gmail.com>
2024-03-25 11:17:25 +0700

    semoga done

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,6 @@ const nextConfig = {
   // reactStrictMode: false,
   // experimental: { optimizeCss: true },  
   images: {
-    dangerouslyAllowSVG: true,
         remotePatterns: [
           {
             protocol: 'https',
